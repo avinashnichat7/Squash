9d894e9c60f3cd4123e65a8ff247bd1a96384172
Avinash <avinash.nichat@neosoftmail.com>
2023-07-24 14:32:24 +0530

    hello world master

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        System.out.println("this is feature demo branch");
-
-        System.out.println("this is feature -demo second commit");
-        System.out.println("this is final commit");
-        System.out.println("this is demo commit");
     }
 }
